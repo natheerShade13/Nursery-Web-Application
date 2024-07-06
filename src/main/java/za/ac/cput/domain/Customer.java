9653932b112c7a1d1ff146de7425adf5a0a1f77a
Natheer13 <natheer.shade@gmail.com>
2024-07-06 19:07:57 +0200

    Follow-up commit.

--- src/main/java/za/ac/cput/domain/Customer.java
+++ src/main/java/za/ac/cput/domain/Customer.java
@@ -5,7 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Customer {
@@ -15,9 +17,122 @@ public class Customer {
     private String firstName;
     private String lastName;
     private String password;
+    private Date dateOfBirth;
     @Embedded
     private Contact contact;
     @OneToMany(mappedBy = "customer")
     private List<Address> addresses;
+
+    protected Customer(){}
+
+    private Customer(Builder builder){
+        this.customerId = builder.customerId;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.password = builder.password;
+        this.dateOfBirth = builder.dateOfBirth;
+        this.contact = builder.contact;
+    }
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return customerId == customer.customerId && Objects.equals(firstName, customer.firstName) && Objects.equals(lastName, customer.lastName) && Objects.equals(password, customer.password) && Objects.equals(dateOfBirth, customer.dateOfBirth) && Objects.equals(contact, customer.contact);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerId, firstName, lastName, password, dateOfBirth, contact);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", date=" + dateOfBirth +
+                ", contact=" + contact +
+                '}';
+    }
+
+    public static class Builder{
+        private long customerId;
+        private String firstName;
+        private String lastName;
+        private String password;
+        private Date dateOfBirth;
+        private Contact contact;
+
+        public Builder setCustomerId(long customerId) {
+            this.customerId = customerId;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setDateOfBirth(Date dateOfBirth) {
+            this.dateOfBirth = dateOfBirth;
+            return this;
+        }
+
+        public Builder setContact(Contact contact) {
+            this.contact = contact;
+            return this;
+        }
+
+        public Builder copy(Customer customer){
+            this.customerId = customer.customerId;
+            this.firstName = customer.firstName;
+            this.lastName = customer.lastName;
+            this.password = customer.password;
+            this.dateOfBirth = customer.dateOfBirth;
+            this.contact = customer.contact;
+            return this;
+        }
+
+        public Customer build(){return new Customer(this);}
+
+    }
 
 }
